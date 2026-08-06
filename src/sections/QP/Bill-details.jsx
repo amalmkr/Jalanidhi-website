d269b34825b7ba18ee5b5727c2e9f1997f8d77cf
amalmkr <amalmkr01@gmail.com>
2026-08-06 21:10:59 +0530

    bill deatils added

--- src/sections/QP/Bill-details.jsx
+++ src/sections/QP/Bill-details.jsx
@@ -1,43 +1,115 @@
 import { FaMoneyBillWave } from "react-icons/fa6";
 import { CiCircleInfo } from "react-icons/ci";
+import { IoSpeedometerOutline } from "react-icons/io5";
+import { FaReceipt } from "react-icons/fa6";
+import { MdOutlineCurrencyRupee } from "react-icons/md";
 function Bill_details(){
     return(
         <>
         <section className="mt-20 p-10 px-10">
             <div className="rounded-xl shadow-2xl p-10 max-w-4xl mx-auto" >
                 <div className="flex items-center justify-center gap-5">
-                    <FaMoneyBillWave className="text-blue-800 text-3xl font-bold"/>
+                    <FaReceipt className="text-blue-800 text-3xl font-bold"/>
                     <h2 className="text-xl md:text-3xl text-blue-800 font-bold">ബില്ല് വിശദാംശം</h2>
                 </div>
-                <div className="flex justify-center mt-10 items-center gap-2">
+                    <hr className="my-8 border-gray-200" />
+
+                <div className="flex justify-center mt-10 items-center gap-2 ">
                     <CiCircleInfo className="text-3xl text-blue-500 font-bold"/>
                     <h4 className="text-center text- md:text-2xl font-medium text-blue-800">Consumer Information</h4>
                 </div>
                 
+
+                <div className="flex justify-between py-2 mt-3">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Consumer Name</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">Amal M</span>
+                </div>
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Consumer Number</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">JN00123</span>
+                </div>
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Mobile Number</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">1234567890</span>
+                </div>
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Area</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">Iringattiri</span>
+                </div>
+
                 <hr className="my-8 border-gray-200" />
 
-                <div className="flex justify-between py-2">
-                    <span className="text-gray-500 font-semibold">Consumer Name</span>
-                    <span className="text-gray-900 font-bold">Amal M</span>
+                <div className="flex justify-center mt-10 items-center gap-2">
+                    <IoSpeedometerOutline className="text-3xl text-blue-500 font-bold"/>
+                    <h4 className="text-center text- md:text-2xl font-medium text-blue-800">Meter Reading</h4>
                 </div>
                 <div className="flex justify-between py-2">
-                    <span className="text-gray-500 font-semibold">Consumer Number</span>
-                    <span className="text-gray-900 font-bold">JN00123</span>
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Previous Reading</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">1830</span>
                 </div>
                 <div className="flex justify-between py-2">
-                    <span className="text-gray-500 font-semibold">Mobile Number</span>
-                    <span className="text-gray-900 font-bold">1234567890</span>
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Current Reading</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">1850</span>
                 </div>
                 <div className="flex justify-between py-2">
-                    <span className="text-gray-500 font-semibold">Area</span>
-                    <span className="text-gray-900 font-bold">Iringattiri</span>
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Water Used</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">20 KL</span>
                 </div>
-
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Bill Month</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">August 2026</span>
+                </div>
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Due Date</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">15 August 2026</span>
+                </div>
+                
                 <hr className="my-8 border-gray-200" />
 
-                <div >
-
+                <div className="flex justify-center mt-10 items-center gap-2">
+                    <FaMoneyBillWave className="text-3xl text-blue-500 font-bold"/>
+                    <h4 className="text-center md:text-2xl font-medium text-blue-800">Charges</h4>
                 </div>
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Minimum Charge</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">100/-</span>
+                </div>
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Water Charge</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">40/-</span>
+                </div>
+                <div className="flex justify-between py-2">
+                    <span className="text-gray-500 font-semibold text-sm md:text-xl">Previous Unpaid Bill</span>
+                    <span className="text-gray-900 font-bold text-sm md:text-xl">0</span>
+                </div>
+
+                <hr className="my-8 border-gray-200"/>
+                
+                <div className="flex justify-between items-center mt-10">
+                    <h4 className="text-2xl font-medium text-blue-800">
+                        Total Amount
+                    </h4>
+
+                    <div className="flex items-center">
+                        <MdOutlineCurrencyRupee className="text-2xl text-green-800" />
+                        <span className="text-3xl font-bold text-green-800">
+                            330
+                        </span>
+                    </div>
+                </div>
+                <div className="flex justify-between mt-5">
+                    <span className="text-gray-500 text-sm md:text-xl">Status</span>
+                    <span>🔴 Unpaid</span>
+                </div>
+                    <button
+                        type="submit"
+                        className="w-full md:w-64 mx-auto block bg-blue-600 hover:bg-blue-700 transition duration-300 text-white font-bold py-3 rounded-lg"
+                    >
+                        Proceed to Pay
+                    </button>
+
+                
+
             </div>
         </section>
         </>
